38f1f85a50dff5639405f1cc51e84ef25c35a710
Tim Sander <tim.sander.business@gmail.com>
2024-09-09 22:52:36 +0200

    test

--- eventmanagement/src/app/app.routes.ts
+++ eventmanagement/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StartseiteComponent } from './startseite/startseite.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StartseiteComponent },   // Leere Route für die Startseite
+    { path: '**', redirectTo: '' },                // Wildcard-Route für ungültige Routen
+];
---
+++ eventmanagement/src/app/startseite/startseite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startseite',
+  standalone: true,
+  imports: [],
+  templateUrl: './startseite.component.html',
+  styleUrl: './startseite.component.scss'
+})
+export class StartseiteComponent {
+
+}
