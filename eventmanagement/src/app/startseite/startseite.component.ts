903f117603c39ea762d9aa714b69591bf79e5e23
Tim Sander <tim.sander.business@gmail.com>
2024-09-10 16:59:45 +0200

    test

--- eventmanagement/src/app/startseite/startseite.component.ts
+++ eventmanagement/src/app/startseite/startseite.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+
+interface AutoCompleteCompleteEvent {
+  originalEvent: Event;
+  query: string;
+}
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [],
+  imports: [
+    AutoCompleteModule, 
+    FormsModule,
+    InputIconModule,
+    IconFieldModule,
+    InputTextModule,
+    FormsModule
+  ],
   templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.scss'
 })
 export class StartseiteComponent {
+
+  items: any[] | undefined;
+
+    selectedItem: any;
+
+    suggestions: any[] = [];
+
+    search(event: AutoCompleteCompleteEvent) {
+        this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+    }
 
 }
